60e8a8c4ced0f6185414a24dddc05b61d641b416
litianliang <litianliang@semptian.com>
2022-09-14 15:26:37 +0800

    update port_config.ini for port start from Ethernet1 to Ethernet32; update led by port name Ethernet1 to Ethernet32;

--- x86_64-semptian_ps7350_32x-r0/plugins/led_control_v1.py
+++ x86_64-semptian_ps7350_32x-r0/plugins/led_control_v1.py
@@ -5,7 +5,6 @@
 
 try:
     from sonic_led.led_control_base import LedControlBase
-    import os
     import syslog
     from socket import *
     from select import *
@@ -22,20 +21,6 @@ def DBG_PRINT(str):
 class LedControl(LedControlBase):
     """Platform specific LED control class"""
 
-    #FILE_PATH='/usr/local/lib/python3.7/dist-packages/sonic_platform'
-    #if os.path.isdir(FILE_PATH):
-        #print("####python3.7 sonic_platform is exsit")
-    #else:
-        #print("####python3.7 sonic_platform is not exsit")
-        #os.system('cp -rf /usr/share/sonic/platform/plugins/sonic_platform /usr/local/lib/python3.7/dist-packages/')
-
-    #FILE_PATH='/usr/local/lib/python2.7/dist-packages/sonic_platform'
-    #if os.path.isdir(FILE_PATH):
-        #print("####python2.7 sonic_platform is exsit")
-    #else:
-        #print("####python2.7 sonic_platform is not exsit")
-        #os.system('cp -rf /usr/share/sonic/platform/plugins/sonic_platform /usr/local/lib/python2.7/dist-packages/')
-        
     # Helper method to map SONiC port name to index
     def _port_name_to_index(self, port_name):
         # Strip "Ethernet" off port name
@@ -75,13 +60,12 @@ class LedControl(LedControlBase):
 
         port_idx = self._port_name_to_index(portname)
         
-        #port_idx=port_idx/4
-        port_idx=port_idx-1
+        port_idx=port_idx//4
         ledMode = self._port_state_to_mode(port_idx, state)
 
 
         port_led=self.f_led  + "port" + str(port_idx) + "_led"
-        #print("DDDDDDD------- port_idx:%s portname:%s state:%s------------------ " % (port_idx, portname, state))
+        print("DDDDDDD------- %d------------------ " % port_idx)
         
         if state == "up":
             port_to_is_present =self.f_led  + "port" + str(port_idx) + "_present"
@@ -99,45 +83,27 @@ class LedControl(LedControlBase):
 
 
         with open(port_led, 'r') as led_file:
-            #saveMode = self._port_state_to_mode(port_idx, led_file.read())
-            saveMode = led_file.read()
+            saveMode = int(led_file.read())
 
-        #print("DDDDDDD------- port_idx:%s portname:%s state:%s saveMode:%s ledMode:%s------------------ " % (port_idx, portname, state, saveMode, ledMode))
         if ledMode == saveMode:
             return
 
         self._port_led_mode_update(port_idx, ledMode)
         DBG_PRINT("update {} led mode from {} to {}".format(portname, saveMode, ledMode))
-        #print("update {} led mode from {} to {}".format(portname, saveMode, ledMode))
 
     # Constructor
 
     def __init__(self):
         self.SONIC_PORT_NAME_PREFIX = "Ethernet"
         self.LED_MODE_UP = [2]
-        self.LED_MODE_DOWN = [0]
+        self.LED_MODE_DOWN = [1]
         
         import os
 
-        sysfs = "cat "
-        sysfs = sysfs + '/sys/class/hwmon/hwmon{0}/name'
-        offset = 3
-        cpld_name = 'xc3400an_port_cpld'
-        for index in range(0, 10):
-            cmd=sysfs.format(index)
-            ret = os.popen(cmd)
-            adaptername=ret.read()
-            
-            print("cmd:%s  adaptername:%s" % (cmd, adaptername))
-            
-            if adaptername.find(cpld_name) >= 0:
-                ret.close()
-                offset = index
-                print("get:%s  adaptername:%s" % (cmd, adaptername))
-                break
-            else:
-                ret.close() 
-            
-        path = '/sys/class/hwmon/hwmon{0}/'
-        BASE_CPLD1_PATH = path.format(offset)
-        self.f_led = BASE_CPLD1_PATH
+        cmd = "ls "
+        cmd = cmd + '/sys/bus/i2c/devices/4-0011/hwmon/'
+        ret = os.popen(cmd)
+        temp ='/sys/bus/i2c/devices/4-0011/hwmon/' +   ret.read()
+        temp = temp[:-1]
+        temp = temp + '/'
+        self.f_led = temp
